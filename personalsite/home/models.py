b175e9f1957c6a456cd7259819dfe4562b7698ca
EllisK-L <Plumpeken@gmail.com>
2021-01-10 02:14:13 -0400

    added start of experience model

--- personalsite/home/models.py
+++ personalsite/home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Experience(models.Model):
+    heading = models.CharField(max_length=30)
+    content = models.CharField(max_length=30000)
+    typeof = models.CharField(max_length=15)
