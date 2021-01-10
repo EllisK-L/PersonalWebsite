43fc61747e2aaff5965f1694375dcba7d3705f24
EllisK-L <Plumpeken@gmail.com>
2021-01-10 17:49:00 -0400

    Added datbase functionality to home page

--- personalsite/home/views.py
+++ personalsite/home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from home.models import Experience
 
 def home(request):
-    return render(request, "home/home.html")
+    experiences = Experience.objects.all()
+    return render(request, "home/home.html", {"experiences":experiences})
