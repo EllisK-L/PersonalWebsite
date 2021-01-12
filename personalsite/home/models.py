66bb465be663c19ec287b0b5d2c857c6cf4de2e5
EllisK-L <Ellis.keener.lacroix@gmail.com>
2021-01-12 01:15:19 -0400

    added some new fields

--- personalsite/home/models.py
+++ personalsite/home/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Experience(models.Model):
-    heading = models.CharField(max_length=30)
-    content = models.CharField(max_length=30000)
+    heading = models.CharField(max_length=125)
+    subHeading = models.CharField(max_length=125, blank=True, null=True)
+    content = models.TextField()
     typeof = models.CharField(max_length=15)
+    photo = models.ImageField(upload_to="home", blank=True, null=True)
